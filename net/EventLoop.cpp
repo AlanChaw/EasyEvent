b7acc923575d51bf2b78e9c9c1e1a920d48b964b
wenbin <wbchaw@qq.com>
2020-02-11 12:48:27 +0000

    pass test 5

--- net/EventLoop.cpp
+++ net/EventLoop.cpp
@@ -4,29 +4,49 @@
 #include "Channel.h"
 
 #include <assert.h>
-#include <poll.h>
 #include <memory>
+
+#include <sys/eventfd.h>
+#include<unistd.h>
+#include <poll.h>
 
 using namespace EasyEvent;
 
 __thread EventLoop* t_loopInThisThread = NULL;
 const int kPollTimeMs = 5000;
 
+static int createEventfd()
+{
+  int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+  assert(evtfd >= 0);
+  return evtfd;
+}
+
+/************************* 构造/析构 ************************/
 EventLoop::EventLoop()
     : _looping(false),
       _quit(false),
+      _callingPendingFunctors(false),
       _threadId(Thread::getCurrentThreadId()),
-      _poller(new Poller(this))
+      _poller(new Poller(this)),
+      _wakeupFd(createEventfd()),
+      _wakeupChannel(new Channel(this, _wakeupFd))
 {
     assert(!t_loopInThisThread);            // 每个线程只能有一个 EventLoop
     t_loopInThisThread = this;
+    _wakeupChannel->setReadCallback(
+        std::bind(&EventLoop::handleRead, this)
+    );
+    _wakeupChannel->enableReading();
 }
 
 EventLoop::~EventLoop(){
     assert(!_looping);
+    ::close(_wakeupFd);
     t_loopInThisThread = NULL;
 }
 
+/************************* 开始/终止 ************************/
 void EventLoop::startLoop(){
     assert(!_looping);
     assert(isInLoopThread());
@@ -40,6 +60,7 @@ void EventLoop::startLoop(){
         for(auto iter = _activeChannels.begin(); iter != _activeChannels.end(); ++iter){
             (*iter)->handleEvent();
         }
+        doPendingFunctors();
     }
 
     _looping = false;
@@ -47,11 +68,68 @@ void EventLoop::startLoop(){
 
 void EventLoop::quitLoop(){
     _quit = true;
+    if(!isInLoopThread()){
+        wakeup();       // 起来！该干活了！
+    }
 }
+
+/************************* 跨线程调用 ************************/
+void EventLoop::runInLoop(const Functor& cb){
+    if(isInLoopThread()){
+        cb();
+    }else{
+        queueInLoop(cb);
+    }
+}
+
+void EventLoop::queueInLoop(const Functor& cb){
+    {
+        LockGuard lk(_mtx);
+        _pendingFunctors.push_back(cb);
+    }
+
+    if(!isInLoopThread() || _callingPendingFunctors){
+        wakeup();
+    }
+}
+
 
 void EventLoop::updateChannel(Channel* channel){
     assert(channel->getOwnerLoop() == this);
     assert(isInLoopThread());
 
     _poller->updateChannel(channel);
+}
+
+// 向 _wakeupFd 写一下以唤醒_wakeupChannel，从而唤醒 EventLoop
+void EventLoop::wakeup(){
+    int one = 1;
+    ssize_t n = ::write(_wakeupFd, &one, sizeof one);
+    assert(n == sizeof one);
+}
+
+
+/************************* private  ************************/
+void EventLoop::handleRead(){
+    int one = 1;
+    ssize_t n = ::read(_wakeupFd, &one, sizeof one);
+    assert(n == sizeof one);
+}
+
+void EventLoop::doPendingFunctors(){
+    std::vector<Functor> functors;
+    _callingPendingFunctors = true;
+    
+    // 换出之后慢慢执行，不长时间持有锁。 相当于move-on-write
+    {
+        LockGuard lk(_mtx);
+        functors.swap(_pendingFunctors);
+    }
+
+    // 按顺序执行所有排队的函数
+    for(size_t i = 0; i < functors.size(); ++i){
+        functors[i]();
+    }
+
+    _callingPendingFunctors = false;
 }
--- net/EventLoop.h
+++ net/EventLoop.h
@@ -2,10 +2,12 @@
 
 #include <vector>
 #include <atomic>
+#include <mutex>
 // #include <memory>
 
 #include "base/Thread.h"
 #include "base/noncopyable.h"
+#include "base/Timestamp.h"
 
 namespace EasyEvent{
 
@@ -16,6 +18,8 @@ class EventLoop : private noncopyable{
 
 public:
     typedef std::vector<Channel*> ChannelList;
+    typedef std::function<void()> Functor;
+    typedef std::lock_guard<std::mutex> LockGuard;
 
     EventLoop();
     ~EventLoop();
@@ -23,16 +27,40 @@ public:
     void startLoop();
     void quitLoop();
 
+    muduo::Timestamp pollReturnTime() const { return _pollReturnTime; }
+
+    // 可安全地跨线程调用
+    void runInLoop(const Functor& cb);
+    void queueInLoop(const Functor& cb);
+
+    // 内部使用
+    void wakeup();
     void updateChannel(Channel* channel);
 
     bool isInLoopThread() const { return _threadId == Thread::getCurrentThreadId();}
 
+    // void runAt();
+    // void runAfter();
+    // void runEvery();
+
 private:
+    void handleRead();
+    void doPendingFunctors();
+
     std::atomic<bool> _looping;
     std::atomic<bool> _quit;
+    std::atomic<bool> _callingPendingFunctors;
+
     Thread::ThreadId _threadId;
     std::unique_ptr<Poller> _poller;
+    muduo::Timestamp _pollReturnTime;
+
+    int _wakeupFd;
+    std::unique_ptr<Channel> _wakeupChannel;
     ChannelList _activeChannels;
+
+    std::mutex _mtx;
+    std::vector<Functor> _pendingFunctors;
 };
 
 }
